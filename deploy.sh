40f6cb29a7f7a5603df9778474da2eaf58adfcee
razar <razear007@gmail.com>
2021-05-28 05:22:55 -0400

    adding deployment configurations

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+docker build -t razear/mega-electric-web-app:latest -t razear/mega-electric-web-app:$SHA -f Dockerfile .
+
+
+docker push razear/mega-electric-web-app:latest
+docker push razear/mega-electric-web-app:$SHA
+
