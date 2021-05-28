42a7e0f2d5a45f71dbd3791ffdf9112ebb7fbbf3
razar <razear007@gmail.com>
2021-05-28 06:46:56 -0400

    adding deployment conf for fandogh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker build -t razear/mega-electric-web-app:latest -t razear/mega-electric-web-
 docker push razear/mega-electric-web-app:latest
 docker push razear/mega-electric-web-app:$SHA
 
-
+fandogh service deploy --image razear/mega-electric-web-app --version latest
