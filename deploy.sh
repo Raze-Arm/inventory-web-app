927215b3515a041375f6c359cb54415a6a3bdd71
razar <razear007@gmail.com>
2021-05-29 06:16:22 -0400

    fixes travis build

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker push razear/mega-electric-web-app:latest
 docker push razear/mega-electric-web-app:$SHA
 
-fandogh service deploy --image razear/mega-electric-web-app --version $SHA --name mega-electric-web-app  --env REACT_APP_HOST=$REACT_APP_HOST -p 3000 -d
+fandogh service deploy --env REACT_APP_HOST="https://mega-electric-app/api/v1" --image razear/mega-electric-web-app --version $SHA --name mega-electric-web-app  -p 3000 -d
