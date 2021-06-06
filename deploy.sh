7cc086751cd7ee6be14279dd45aabd0562a6c201
razar <razear007@gmail.com>
2021-06-06 04:22:01 -0400

    fixing travis build

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ docker push razear/mega-electric-web-app:$SHA
 
 
 fandogh  secret create --name backend-api  -t environment-secret -f SECRET_KEY=$BACKEND_API
-fandogh  secret put --name backend-api  -t environment-secret -f SECRET_KEY=$BACKEND_API
+#fandogh  secret put --name backend-api  -t environment-secret -f SECRET_KEY=$BACKEND_API
 fandogh service apply -f web-app-deployment.yml  \
                  -p SHA=$SHA
 #fandogh service deploy  --image razear/mega-electric-web-app --version $SHA --name mega-electric-web-app  -p 3000 -d
